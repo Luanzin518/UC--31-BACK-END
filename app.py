f6d271480fd9301c4e5ae377c1be8a4cdd2a1346
Luanzin518 <gpsnascimento2022@gmail.com>
2026-05-21 08:35:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,9 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def inicio():
-    return render_template("index.html")
-
-
-@app.route("/lanches")
-def lanches():
-    return render_template("lanche.html")
-
-
-@app.route("/pedidos")
-def pedidos():
-    return render_template("pedidos.html")
-
-
-@app.route("/contato")
-def contato():
-    return render_template("contato.html")
-
-@app.route("/cardapio")
-def sobre():
-    return render_template("cardapio.html")
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
