c26d23eeaef953eecac3589946dceb4cc7db9d52
Luanzin518 <gpsnascimento2022@gmail.com>
2026-05-27 17:12:44 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/recebedados', methods=['POST'])
+def recebedados():
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+
+    return "{} e {}".format(nome, email)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/formregistrar')
+def formregistrar():
+    return render_template('registrar.html')
+
+
+@app.route('/registrar', methods=['POST'])
+def registrar():
+    usuario = request.form['usuario']
+    email = request.form['email']
+    senha = request.form['senha']
+    confirmar = request.form['confirmar']
+
+    if senha == confirmar:
+        return "Usuário registrado com sucesso"
+    else:
+        return "As senhas não conferem"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
